5604d7aea5254ee8c1f98554fbe39c8d679032c8
mdataurrahman555 <74402921+mdataurrahman555@users.noreply.github.com>
2022-04-17 01:50:04 +0600

    footer updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const date = new Date();
+  const year = date.getFullYear();
+  return (
+    <footer>
+      <Container>
+        <div className="inner-footer">
+          <div className="footer-links">
+            <Link to="/aboutme">AboutMe</Link>
+            <Link to="/blogs">Blogs</Link>
+          </div>
+          <h5 className="footer-text">All Copyrights reserved &copy; {year}</h5>
+        </div>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
