2523b18e5f5520c9ea1356a08c0a1e8ec62b58a6
mdataurrahman555 <74402921+mdataurrahman555@users.noreply.github.com>
2022-04-17 00:57:52 +0600

    banner updated

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div className="banner-section">
+      <div className=".container">
+        <div className="inner-banner-section">
+          <div>
+            <h1>Hello, My Name Is Ava Ostin</h1>
+            <h4>I'm A Gym Trainer</h4>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, sit
+              obcaecati. Fugiat, quasi, voluptate error iste quam fugit impedit
+              rerum quia dolorum harum repellat doloribus eum magni quaerat illo
+              quo.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
