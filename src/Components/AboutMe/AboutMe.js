077829638cdcdd505110a667597f98717b33553d
mdataurrahman555 <74402921+mdataurrahman555@users.noreply.github.com>
2022-04-18 15:46:54 +0600

    blogs and about section updated

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import "./AboutMe.css";
 
 const AboutMe = () => {
-  return <div>AboutMe</div>;
+  return (
+    <Container>
+      <div className="about-container">
+        <h2 className="about-title">Hi, My Name is Md Ataur Rahman</h2>
+        <p className="about-text">
+          I'm still learning programming. And I want to learn.For the next 4-5
+          months I have been focusing on what I want to be a full stack
+          developer.And Then I hope to do a remote job. I have to work hard to
+          get these. That's why I still do Hark work.
+          <br />
+          Happy Coding
+        </p>
+      </div>
+      ;
+    </Container>
+  );
 };
 
 export default AboutMe;
--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -1,7 +1,51 @@
 import React from "react";
+import "./Blogs.css";
 
 const Blogs = () => {
-  return <div>Blogs</div>;
+  return (
+    <section id="questions-section">
+      <div class="question-div">
+        <h3 class="quenstion-name">
+          Difference between authorization and authentication ?
+        </h3>
+        <p class="answer-of-question">
+          Authentication: Authentication verifies who the user is.Authentication
+          ব্যবহারকারীর দ্বারা দৃশ্যমান এবং আংশিকভাবে পরিবর্তন করা যায় ।
+          Authentication Methods মধ্যে একটা হলো password, pin |
+          <br />
+          Authorization: Authorization determines what resources a user can
+          access.Authorization সে কি এটার এক্সেস চাই কিনা সেটা verify করতে পারে
+          Surely | Authorization always takes place after authentication.
+          Authorization isn't visible to or changeable by the user.
+        </p>
+      </div>
+      <div class="question-div">
+        <h3 class="quenstion-name">
+          Why are you using firebase? What other options do you have to
+          implement authentication?
+        </h3>
+        <p class="answer-of-question">
+          We are using firebase becuase firebase মাধ্যমে বিভিন্ন ধরণের
+          Authentication system দিয়ে আমরা আমাদের বিভিন্ন Application করেতে পারি
+          |
+          <br />
+          Best Open Source Firebase Alternatives : Back4App, AWS Amplify,
+          Kuzzle,Couchbase, NativeScript, RxDB, Flutter etc
+        </p>
+      </div>
+      <div class="question-div">
+        <h3 class="quenstion-name">
+          What other services does firebase provide other than authentication ?
+        </h3>
+        <p class="answer-of-question">
+          Firebase মাধ্যমে আমরা যে যে কাজ করতে পারি authentication ছাড়া Emulator
+          suite, Realtime databse, cloud firestore storage, Machine larning,
+          Hosting, Cloud function. এই এই জিনিস গুলো আমরা realtime কাজে লাগাতে
+          পারি |
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Blogs;
